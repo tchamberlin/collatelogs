4ab2f1b5c35bf8a8d22eb0f7a09902b28302c74a
Thomas Chamberlin <tchamberlin@users.noreply.github.com>
2018-05-04 11:29:09 -0400

    Forgot version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='collatelogs',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple log collator',
     url='http://github.com/tchamberlin/collatelogs',
     author='Thomas Chamberlin',
